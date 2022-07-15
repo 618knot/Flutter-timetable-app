68ec8d888405e6dfa919ccb9bf3618809d9e2f3a
618knot <knot618@gmail.com>
2022-07-16 01:20:11 +0900

    lectureSchedule.dartに名前変更 テスト用定数リストを用意 main.dartから時間割を表示できるように変更

--- lib/lectureSchedule.dart
+++ lib/lectureSchedule.dart
@@ -1,11 +1,120 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class LectureSchedule extends StatelessWidget {
+  const LectureSchedule({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final List<String> lectureNameList = [
+      "エレクトロニクス基礎",
+      " ",
+      " 情報技術概論",
+      "教職概論(教職)",
+      "えいごいっぱい",
+      "化学基礎",
+      "スポーツ総合2",
+      "情報技術概論",
+      "なんかいっぱい",
+      "線形代数学I",
+      "生物学入門",
+      "微分積分学II",
+      "地域課題プロジェクト",
+      "情報学",
+      "データ活用基礎",
+      "キャリア形成A2",
+      "言語リテラシー2",
+      "地域課題プロジェクト",
+      "えいごいっぱい",
+      " ",
+      "物理学基礎",
+      " ",
+      "特別支援教育学",
+      "微分積分学I",
+      " "
+    ];
+    final List<String> roomList = [
+      "B101",
+      " ",
+      "B101, G201, G202, ZOOM",
+      "B204",
+      "どっか",
+      "B201",
+      "C111(体育館)",
+      "B101, G201, G202, ZOOM",
+      "どっか",
+      "B204",
+      "B204",
+      "B101",
+      "H101, ZOOM",
+      "B102",
+      "G201, G202",
+      "B101, H101",
+      "B204",
+      "H101, ZOOM",
+      "どっか",
+      " ",
+      "オンデマンド",
+      " ",
+      "B203",
+      "B201",
+      " "
+    ];
+    final List<String> teacherList = [
+      "山中",
+      " ",
+      "小松川、砂原、丸太、木滑",
+      "宮嶋",
+      "いっぱい",
+      "高田",
+      "今野",
+      "小松川、砂原、丸太、木滑",
+      "いっぱい",
+      "藤井",
+      "井手、木村、平井",
+      "山中、本多",
+      "石田、磯部、山下、川名",
+      "萩原、石田、高野(泰)、山川",
+      "山中、本多",
+      "石田",
+      "山中、山下",
+      "石田、磯部、山下、川名",
+      "いっぱい",
+      " ",
+      "福田(誠)",
+      " ",
+      "飯塚、藤根、荒木",
+      "藤井",
+      " ",
+    ];
+    final List<int> lectureTypeList = [
+      1,
+      0,
+      1,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      1,
+      0,
+      1,
+      1,
+      0,
+      1,
+      1,
+      1,
+      1,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ];
+    return lectureSchedule(
+        context, lectureNameList, roomList, teacherList, lectureTypeList);
   }
 
   Widget lectureSchedule(context, List? lectureNameList, List? roomList,
