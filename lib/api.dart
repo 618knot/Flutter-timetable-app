d75a33cbdfca2581ca99f70e285d6d0c06d27f60
618knot <knot618@gmail.com>
2022-08-29 23:40:03 +0900

    apiの呼び出し

---
+++ lib/api.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() {
+  callAPI();
+}
+
+class requestData {
+  String? content;
+  String? date;
+
+  requestData({this.content, this.date});
+  Map<String, dynamic> toJson() => {'content': content, 'date': date};
+}
+
+Future callAPI() async {
+  var url =
+      Uri.parse('https://timetable-backend-kn.herokuapp.com/api/v1/get/csv/');
+
+  var request = new requestData(content: "goC.csv", date: "2022-08-29");
+
+  final response =
+      await http.post(url, body: json.encode(request.toJson()), headers: {
+    'Content-Type': 'application/json',
+  });
+
+  if (response.statusCode == 200) {
+    Map data = json.decode(response.body);
+    return data;
+  } else {
+    throw Exception('Failed');
+  }
+}
+
